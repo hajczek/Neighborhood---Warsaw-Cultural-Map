14fce61500b8095af504aecca483134155a6869b
hajczek <i.hajczewska@gmail.com>
2018-05-24 18:15:57 +0200

    feat: add functions to displayed choosen markers on map

--- src/App.js
+++ src/App.js
@@ -8,28 +8,20 @@ import Cinemas from './data/cinemas.json'
 import Museums from './data/museums.json'
 import Galeries from './data/galeries.json'
 
-let markers = [];
+let markers_all = [];
 let markers_theatres = [];
 let markers_cinemas = [];
 let markers_museums = [];
 let markers_galeries = [];
 
 let theatres = Theatres;
-    let cinemas = Cinemas;
-    let museums = Museums;
-    let galeries = Galeries;
+let cinemas = Cinemas;
+let museums = Museums;
+let galeries = Galeries;
 
 class App extends Component {
   state = {
-        markers: [],
-       //markers_theatres: [],
-        markers_cinemas: [],
-        markers_museums: [],
-        markers_gakeries: [],
-        // Theatres,
-        Cinemas,
-        Museums,
-        Galeries,
+        markers: markers_all,
         pageTitle: "Warsaw Cultural Map",
         listTitle: "List of Places",
         activeKey: ""
@@ -41,63 +33,97 @@ class App extends Component {
     });
   };
 
-  showTheatres = (markers) => {
-      this.setState({theatres});
-      console.log(theatres)
+ componentWillMount() {
+
+    for (let i = 0; i < theatres.length; i++) {
+      let marker = theatres[i];
+    // Push the marker to our array of markers.
+    markers_all.push(marker);
+    markers_theatres.push(marker);
   }
+
+  for (let i = 0; i < cinemas.length; i++) {
+    let marker = cinemas[i];
+    // Push the marker to our array of markers.
+    markers_all.push(marker);
+    markers_cinemas.push(marker);
+  }
+
+  for (let i = 0; i < museums.length; i++) {
+    let marker = museums[i];
+    // Push the marker to our array of markers.
+    markers_all.push(marker);
+    markers_museums.push(marker);
+  }
+
+  for (let i = 0; i < galeries.length; i++) {
+    let marker = galeries[i];
+    // Push the marker to our array of markers.
+    markers_all.push(marker);
+    markers_galeries.push(marker);
+ }
+}
+
+showTheatres = (markers) => {
+  this.setState({markers: markers_theatres});
+  console.log(markers_theatres);
+} 
+
+showCinemas = (markers) => {
+  this.setState({markers: markers_cinemas});
+  console.log(markers_cinemas);
+}
+
+showMuseums = (markers) => {
+  this.setState({markers: markers_museums});
+  console.log(markers_museums);
+}
+
+showGaleries = (markers) => {
+  this.setState({markers: markers_galeries});
+  console.log(markers_galeries);
+}
+
+showAll = (markers) => {
+  this.setState({markers: markers_all});
+  console.log(markers_all);
+}
 
   render() {
    // The following group uses the location array to create an array of markers on initialize.
-
-      for (let i = 0; i < theatres.length; i++) {
-         let marker = theatres[i];
-        // Push the marker to our array of markers.
-        markers_theatres.push(marker);
-        markers.push(marker);
-      }
-
-      for (let i = 0; i < cinemas.length; i++) {
-        let marker = cinemas[i];
-        // Push the marker to our array of markers.
-        markers_cinemas.push(marker);
-        markers.push(marker);
-      }
-
-      for (let i = 0; i < museums.length; i++) {
-       let marker = museums[i];
-        // Push the marker to our array of markers.
-        markers_museums.push(marker);
-        markers.push(marker);
-      }
-
-      for (let i = 0; i < galeries.length; i++) {
-        let marker = galeries[i];
-        // Push the marker to our array of markers.
-        markers_galeries.push(marker);
-        markers.push(marker);
-      }
 
   return (
       <div className="container">
         <h1>{this.state.pageTitle}</h1>
         <div className="options-box">
           <button onClick={() => this.showTheatres()} id="show-theatres">Show Theatres</button>
-          <button id="show-cinemas">Show Cinemas</button>
-          <button id="show-museums">Show Museums</button>
-          <button id="show-galeries">Show Galeries</button>
-          <button id="show-all">Show All Places</button>
+          <button onClick={() => this.showCinemas()} id="show-cinemas">Show Cinemas</button>
+          <button onClick={() => this.showMuseums()} id="show-museums">Show Museums</button>
+          <button onClick={() => this.showGaleries()} id="show-galeries">Show Galeries</button>
+          <button onClick={() => this.showAll()} id="show-all">Show All Places</button>
         </div>
         <div id="list-of-localisations">
           <h2>{this.state.listTitle}</h2>
-            <Places onShowTheatres={this.showTheatres} markers={markers} />
+            <Places 
+              onShowTheatres={this.showTheatres} 
+              onShowCinemas={this.showCinemas}
+              onShowMuseums={this.showMuseums}
+              onShowGaleries={this.showGaleries}
+              onShowAll={this.showAll} 
+              markers={this.state.markers} 
+            />
         </div>
         <div id="map">
           <Map
             activeKey={this.state.activeKey}
             toggleLocationsActive={this.toggleLocationsActive}
-            onShowTheatres={this.showTheatres}
             isMarkerShown
-            markers={markers}
+            onShowTheatres={this.showTheatres}
+            onShowCinemas={this.showCinemas}
+            onShowMuseums={this.showMuseums}
+            onShowGaleries={this.showGaleries}
+            onShowAll={this.showAll}
+            markers={this.state.markers}
             googleMapURL="http://maps.googleapis.com/maps/api/js?libraries=geometry,drawing,places&key=AIzaSyBqtLvddq3jzZ_Lnu9M8266EMVBfXtlUT4"
             loadingElement={<div style={{ height: `100%` }} />}
             containerElement={<div style={{ height: `100%` }} />}
