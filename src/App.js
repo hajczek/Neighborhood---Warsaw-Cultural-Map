c669f541286b3ddddcb4d99ff74d5cf6ac4e519f
hajczek <i.hajczewska@gmail.com>
2018-05-30 16:35:18 +0200

    feat: add google api key

--- src/App.js
+++ src/App.js
@@ -184,7 +184,7 @@ render() {
             onShowGaleries={this.showGaleries}
             onShowAll={this.showAll}
             markers={this.state.markers}
-            googleMapURL="http://maps.googleapis.com/maps/api/js?libraries=places&key=YOUR-GOOGLE-MAPS-API-KEY"
+            googleMapURL="http://maps.googleapis.com/maps/api/js?libraries=places&key=AIzaSyBqtLvddq3jzZ_Lnu9M8266EMVBfXtlUT4"
             loadingElement={<div style={{ height: `100%` }} />}
             containerElement={<div style={{ height: `100%` }} />}
             mapElement={<div style={{ height: `100%` }} />}
